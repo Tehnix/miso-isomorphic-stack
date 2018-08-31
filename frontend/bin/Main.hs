b20afaa5e30afaaf855dfa2d4d5e0cbc6faa6a26
Christian Kjaer Laustsen <ckl@codetalk.io>
2018-08-31 16:42:03 +0200

    Add temporary fix for JSAddle duplicating the views

--- frontend/bin/Main.hs
+++ frontend/bin/Main.hs
@@ -11,12 +11,14 @@ import qualified Frontend.Update as Frontend
 main :: IO ()
 main =
   JSaddle.run 8080 $ do
-    currentURI <- Miso.getCurrentURI
-    Miso.startApp App { initialAction = Common.NoOp
-                      , model = Common.initialModel currentURI
-                      , update = Miso.fromTransition . Frontend.updateModel
-                      , view = Common.viewModel
-                      , events = Miso.defaultEvents
-                      , subs = [Miso.uriSub Common.HandleURI]
-                      , mountPoint = Nothing
-                      }
+    -- FIXME: Once https://github.com/Tehnix/miso-isomorphic-stack/issues/6 is fixed, switch back to Miso.startApp.
+    -- currentURI <- Miso.getCurrentURI
+    Miso.miso $ \currentURI -> App
+      { initialAction = Common.NoOp
+      , model = Common.initialModel currentURI
+      , update = Miso.fromTransition . Frontend.updateModel
+      , view = Common.viewModel
+      , events = Miso.defaultEvents
+      , subs = [Miso.uriSub Common.HandleURI]
+      , mountPoint = Nothing
+      }
